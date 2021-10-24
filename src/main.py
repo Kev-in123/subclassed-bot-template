d02b6d3ea0955ac6a666d88206994e264749e618
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-10-24 13:43:32 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import discord
 from bot import Bot
 
+# bot constructor, edit this to your needs.
 bot = Bot(
     command_prefix = "!",
     activity = discord.Game("!help")
   )
 
-bot.run("TOKEN")
+bot.run("TOKEN") #  change `TOKEN` to your bot's token
+# running a bot like this is not recommended (token is visible in the source) but for the sake of this template, it's gonna be like this
